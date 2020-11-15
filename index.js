4ac5a0465c09bb79184311b0101d948bb05edb3e
Anna Kuznetsova1 <anna_kuznetsova1@epam.com>
2020-11-15 19:52:59 +0300

    add task3

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path');
 require('http')
 .Server((req, res) => {
     const CORS = {
@@ -7,17 +9,31 @@ require('http')
         'Access-Control-Allow-Methods': 'GET,POST,DELETE',
         'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
     };
-    
+
     res.writeHead(200, CORS);
 
     if (req.url === '/login/') {
         return res.end('itmo286135');
-      } else if(req.url === '/sample/') {
+      } else if(req.url === '/promise/') {
         return res.end((function task(x) {
-          return x * this ** 2
+          return new Promise((resolve, reject) => {
+            if (x < 18) { resolve('yes')} else { reject('no') }
+          })
         }).toString())
+      } else if (req.url === '/fetch/') {
+        const filePath = path.join(__dirname, 'index.html');
+    
+        res.writeHead(200, {
+          ...CORS,
+          'Content-Type': 'text/html'
+        });
+    
+        fs.readFile(filePath, (err, html) => {
+          res.write(html);
+          res.end();
+        });
+      } else {
+        res.end('itmo286135');
       }
-  
-    res.end('itmo286135');
 })
 .listen(process.env.PORT);
