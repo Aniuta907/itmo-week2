62c7546ca5c63f0769d01e09fb5acecc1d7ffc9b
Anna Kuznetsova1 <anna_kuznetsova1@epam.com>
2020-11-21 15:03:04 +0300

    add task4

--- index.js
+++ index.js
@@ -4,36 +4,24 @@ require('http')
 .Server((req, res) => {
     const CORS = {
         'Access-Control-Allow-Origin': '*',
-        'X-Author' : 'itmo286135',
-        'Content-Type': 'text/plain; charset=UTF-8',
-        'Access-Control-Allow-Methods': 'GET,POST,DELETE',
-        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
     };
 
     res.writeHead(200, CORS);
 
-    if (req.url === '/login/') {
-        return res.end('itmo286135');
-      } else if(req.url === '/promise/') {
-        return res.end((function task(x) {
-          return new Promise((resolve, reject) => {
-            if (x < 18) { resolve('yes')} else { reject('no') }
-          })
-        }).toString())
-      } else if (req.url === '/fetch/') {
-        const filePath = path.join(__dirname, 'index.html');
-    
-        res.writeHead(200, {
-          ...CORS,
-          'Content-Type': 'text/html'
-        });
-    
-        fs.readFile(filePath, (err, html) => {
-          res.write(html);
-          res.end();
-        });
-      } else {
-        res.end('itmo286135');
-      }
+    if (req.url === '/result4/') {
+      const data = { message: 'itmo286135', 'x-result': req.headers['x-test']}
+  
+      let body = '';
+      req.on('data', function (chunk) {
+        body += chunk;
+      }).on('end', () => {
+        return res.end(JSON.stringify({...data, 'x-body': body}))
+      })
+    } else {
+      res.end('itmo286135');
+    }
 })
 .listen(process.env.PORT);
